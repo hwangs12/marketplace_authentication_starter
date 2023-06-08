4074274001e69f1f8c992a915a8d8f79ebf75fb7
Sunyong <sunyong2021@gmail.com>
2023-06-08 11:47:10 -0700

    fixed confirmpassd error

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,11 +1,78 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, Model } from "mongoose";
+const bcrypt = require("bcrypt");
 
 interface IUser {
-  info: string;
+  name: string;
+  email: string;
+  password: string;
+  confirmpassword: String;
+  comparePassword(password: string): Promise<boolean>;
+}
+
+interface IUserModel extends Model<IUser> {
+  isThisEmailInUse(email: string): Promise<boolean>;
 }
 
 const userSchema = new Schema<IUser>({
-  info: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  // confirmpassword: { type: String, required: true },
 });
 
-export const User = model<IUser>("User", userSchema);
+userSchema.pre("save", function (next) {
+  if (this.isModified("password") || this.isModified("email")) {
+    if (this.isModified("password")) {
+      bcrypt.hash(this.password, 8, (err, hash) => {
+        if (err) return next(err);
+        this.password = hash;
+        // bcrypt.hash(this.confirmpassword, 8, (err, hash) => {
+        //   if (err) return next(err);
+        //   this.confirmpassword = hash;
+        if (this.isModified("email")) {
+          bcrypt.hash(this.email, 8, (err, hash) => {
+            if (err) return next(err);
+            this.email = hash;
+            next();
+          });
+        } else {
+          next();
+        }
+      });
+    } else {
+      bcrypt.hash(this.email, 8, (err, hash) => {
+        if (err) return next(err);
+        this.email = hash;
+        next();
+      });
+    }
+  } else {
+    next();
+  }
+});
+
+//email/pw > encrypt to hash, user email/pw in RN and encrypt > send to auth  > decrypt > compare
+userSchema.methods.comparePassword = async function (password) {
+  if (!password) throw new Error("Password is missing");
+
+  try {
+    const result = await bcrypt.compare(password, this.password);
+    return result;
+  } catch (error: any) {
+    console.log("Error while compaing password", error.message);
+  }
+};
+
+userSchema.statics.isThisEmailInUse = async function (email: string) {
+  if (!email) throw new Error("Invalid Email");
+  try {
+    const user = await this.findOne({ email: email });
+    if (user) return false;
+    return true;
+  } catch (error: any) {
+    console.log("error inside isThisEmailInUse method", error.message);
+    return false;
+  }
+};
+
+export const User = model<IUser, IUserModel>("User", userSchema);
